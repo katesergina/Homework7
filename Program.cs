49bcf38aa7c86df35f66e35b7051a7b25a7fb6c1
Ekaterina Sergina <111311879+katesergina@users.noreply.github.com>
2022-09-30 09:32:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -90,3 +90,71 @@ foreach (var item in arr)
     Console.WriteLine(item);
 }
 Console.WriteLine(Contains(arr, 15));
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int rows = 3;
+int columns = 4;
+
+int[,] GetArray(int m, int n)
+{
+    var result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j =0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(100);
+        }
+    }
+    return result;
+}
+
+bool Contains(int[,] arr, int element)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == element)
+            {
+                return true;
+            }
+            
+        }
+    }
+    return false;
+}
+
+double[] GetAveragePerColumn(int[,] arr)
+{
+    var result = new double[arr.GetLength(1)];
+
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum += arr[j, i];
+        }
+        result[i] = Math.Round(sum / arr.GetLength(0), 2);
+    }
+
+    return result;
+}
+
+void PrintArray(double[,] arr)
+{
+     for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+foreach (var item in GetAveragePerColumn(GetArray(rows, columns)))
+{
+    Console.WriteLine(item);
+}
